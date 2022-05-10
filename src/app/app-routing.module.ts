add33f26e94b515fb9ac4e5f42fe5627e64ca93e
Thanh Tungg <trantungg910@gmail.com>
2022-05-10 08:13:06 +0700

    init project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+
+const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: 'hello', component: HelloComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
