8af5e7508571323dc7b99fd3b0a239c315726106
Thanh Tungg <trantungg910@gmail.com>
2022-05-30 16:39:54 +0700

    feat: search

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import Isubject from 'src/app/models/subject';
+import { SubjectsService } from 'src/app/services/subjects.service';
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss'],
+})
+export class SearchComponent implements OnInit {
+  loading = false;
+  visible = false;
+  data: Array<Isubject> = [];
+  constructor(private subjectsService: SubjectsService) {}
+
+  ngOnInit(): void {}
+  handlerSearch(data: NgForm) {
+    this.subjectsService.searchText(data.value.textSearch).subscribe((data) => {
+      console.log(
+        '🚀 => SearchComponent => this.subjectsService.searchText => data',
+        data
+      );
+      return (this.data = data);
+    });
+  }
+}
